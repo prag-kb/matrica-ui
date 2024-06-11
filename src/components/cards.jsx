5f2ad0d42521b6e34504da5506715584ae621cc1
prag-kb <kbprabu6@gmail.com>
2024-06-11 17:43:36 +0530

    css file temp fix

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -6,7 +6,7 @@ const Cards = () => {
   return (
       <div style={{display: "grid", gridTemplateColumns:"0fr 0fr 1fr", gap:"15px", margin:"20px 0 0 20px"}}>
         <div>
-          <div className='cardBody'>
+          <div className='cardBody' style={{ width: "12rem",backgroundColor:"lightgrey",padding:"10px",borderRadius:"10px",height:"10rem",textAlign:"center",display:"grid"}}>
             <div style={{color:"slategray"}}>Total Portfolio Value</div>
             <div style={{fontSize:"xx-large"}}>$ 176.30</div>
             <select style={{margin:"10px"}}></select>
@@ -14,7 +14,7 @@ const Cards = () => {
         </div>
 
         <div>
-          <div className='cardBody'>
+          <div className='cardBody' style={{ width: "12rem",backgroundColor:"lightgrey",padding:"10px",borderRadius:"10px",height:"10rem",textAlign:"center",display:"grid"}}>
             <div style={{color:"slategray"}}>NFTs Owned</div>
             <div style={{fontSize:"xx-large"}}>17</div>
             <div className=""></div>
